b11fd88cbec0a8a7ddaa7448f7862569a2a77f9b
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-10-24 20:02:13 +0200

    initial commit

---
+++ print.py
@@ -0,0 +1,11 @@
+#printing a greeting message
+print("hello world!")
+
+#printing my name
+print("My name is Alex")
+
+#printing a fun fact about myself
+print("I love playying soccer")
+
+#performing and printing a simple calculation
+print("The result of 3 + 5 is:",3 + 5)
