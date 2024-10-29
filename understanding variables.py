05146110040449d8bae9d57741471d90ffd85fec
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-10-29 20:09:35 +0200

    variables commit

---
+++ understanding variables.py
@@ -0,0 +1,11 @@
+#Creatig different variablles
+x = 5
+y = "Jhon"
+print(x)
+print(y)
+
+codingal = "codingal"
+
+# user imput
+name = input("Enter your name")
+print("/nHello", name , "/nwelcome to codingal")
