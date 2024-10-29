ef8092520bb03da34c27118d332c14e49bd0f2ba
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-10-29 19:57:55 +0200

    space commit

---
+++ welcome to the art of coding.py
@@ -0,0 +1,14 @@
+#print a sentence
+print ("welcome to the world of programing!")
+
+#print a number
+print("4")
+
+#next line after print
+print("Hello Python /n")
+
+#pass multiple arguments for printing
+print("Hello",5)
+
+#End argument of print Statement
+print("Welcome to",end="*")
