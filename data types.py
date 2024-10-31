2249ed7dd0c90b5ff8bc7044ced6c2ee45525d2e
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-10-31 20:18:33 +0200

    types of data commit

---
+++ data types.py
@@ -0,0 +1,12 @@
+# Lets check the datatypes of different values
+a = 5
+print("type of a:" , type(a))
+
+b=2,5
+print("type b:" , type(b))
+
+c="coding"
+print("type of c:" ,type(c))
+
+d= True
+print("type of d:" ,type(d))
--- storing birthdays.py
+++
@@ -1,5 +0,0 @@
-#print a birth date
-print("17 october 2011")
-
-#print sentence
-print("/n Happy Birth Day :)")
