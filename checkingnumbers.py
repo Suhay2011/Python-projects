d607d3bba6991b10cc51b3b11cf36290feaf733b
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-11-07 19:54:17 +0200

    numbers commit

---
+++ checkingnumbers.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a Number :"))
+if (i < 15):
+ print("i is smaller than 15")
+ print("i'm an if Block")
+else:
+ print("i is greate than 15")
+ print("i'm in else Block")
+ print("i'm not an if Block and not a else Block")
