5438af893f8443b8e88c9cfa9d0c2918facee7dd
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-11-07 19:39:53 +0200

    loss commit

--- conditional statements.py
+++ conditional statements.py
@@ -1,8 +1,7 @@
 num = 3
 if num > 0:
-    print(num,"is a positive number.")
+print(num,"is a positive number.")
 
-
-    num = -1
-    if num > 0:
-        print(num, "is positive number.")
+num = -1
+if num > 0:
+print(num, "is positive number.")
---
+++ profit loss.py
@@ -0,0 +1,8 @@
+actual_cost = float(input("Please Enter an actual Product Selling price :"))
+sale_amount = float(input("Please Enter a Sale Amount :"))
+
+if (sale_amount  > actual_cost):
+amount = sale_amount - actual_cost
+print("Total Profit = {0}".format(amount))
+else:
+print("NO PROFIT!")
