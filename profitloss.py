2d7e96b532ac100124d141029a0a82e9a08c77cc
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-11-07 19:47:16 +0200

    profit commit

--- profitloss.py
+++ profitloss.py
@@ -2,7 +2,8 @@ actual_cost = float(input("Please Enter an actual Product Selling price :"))
 sale_amount = float(input("Please Enter a Sale Amount :"))
 
 if (sale_amount  > actual_cost):
-amount = sale_amount - actual_cost
-print("Total Profit = {0}".format(amount))
+ 
+ amount = sale_amount - actual_cost
+ print("Total Profit = {0}".format(amount))
 else:
-print("NO PROFIT!")
+ print("NO PROFIT!")
