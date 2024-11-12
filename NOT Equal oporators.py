aa35bb433078bee4f00c4c97802f4270f09f2824
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-11-12 19:58:03 +0200

    Equil commit

---
+++ NOT Equal oporators.py
@@ -0,0 +1,18 @@
+a = 10
+b = 12
+c = 12
+
+print( a !=b)
+print( b !=c)
+
+a = "Python"
+b = "Coding"
+
+if a !=b:
+    print(a, 'and',b,'are different')
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+  print('Hello')
