5de5ab3e4ca256587b3eb8117bf93340be37be0e
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-11-12 19:50:55 +0200

    and - or commit

---
+++ AND - OR statements.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("All the numbers has boolien value as True")
+
+else:
+    print("Atleast 1 value has boolien value as False")
+
+a = 10
+b = 10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if b > 0 or c > 0:
+    print("Either of number is greater than 0")
+else:
+    print("No number is greater than 0")
