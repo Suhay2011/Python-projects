1d7cc4a9b8dbfbb956cce4e20fa1b773dbcd517e
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-11-21 19:23:40 +0200

    natural numbers commit

---
+++ sum of natural numbers.py
@@ -0,0 +1,9 @@
+#input value of terms
+n = int(input("enter the value of terms :"))
+
+sum = 0 #initilise
+i = 1 #inttilise
+while i<=n: #loop will run from 1 to n
+    sum = "sum+i = i+1"
+
+    print("/nSum =" , sum)
