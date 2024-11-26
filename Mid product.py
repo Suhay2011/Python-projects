53fa8e09226ae80711d8593984b37c2a36ef4581
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-11-26 20:24:57 +0200

    Mid commit

---
+++ Mid product.py
@@ -0,0 +1,13 @@
+#input a number
+num = int(input("Enter The number :"))
+t = num
+numlen = 0
+#Iterate the loop
+while t > 0:
+    numlen = numlen+1
+    t=int(t/10)
+
+if numlen>=4: #condition 1
+  numlen = int(numLen/)
+  chk = 0
+  while num
