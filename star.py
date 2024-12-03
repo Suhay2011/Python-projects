76da71babe676ff608e0e5f5d7e74698031d83d5
Suhay2011 <suhayl.abrahams3@gmail.com>
2024-12-03 20:02:46 +0200

    star commit

---
+++ star.py
@@ -0,0 +1,26 @@
+import turtle
+
+turtle.Screen().bgcolor("aqua")
+board = turtle.Turtle()
+
+#first triangle for star
+board.forward(100) #draw base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+boared.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+
+# second triangle for star
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(100)
+board.forward(120)
