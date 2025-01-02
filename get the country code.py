8725bf8573df4bba4c4793071e8cf1a100674db4
Suhay2011 <suhayl.abrahams3@gmail.com>
2025-01-02 19:50:08 +0200

    country code commit

---
+++ get the country code.py
@@ -0,0 +1,11 @@
+SPCMcountry_code = {'India' : '0091',
+                    'Australia' : '0025'
+                    'Nepal'  '00977'}
+
+#Search dictionary for country code of India
+print("Country code for India is -")
+print(Country_code.get('India' , 'Not Found'))
+
+#Search dictionary for country code of Japan
+print("Country code for Japan is -")
+print(Country_code.get('Japan' , 'Not Found'))
