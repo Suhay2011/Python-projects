6fc5b9a5ce86809681b044d0afd431b0f60388d2
Suhay2011 <suhayl.abrahams3@gmail.com>
2025-01-09 19:53:13 +0200

    exit commit

---
+++ That's the exit.py
@@ -0,0 +1,15 @@
+#Python program to demonstrate
+#exit ()
+
+for i in range(10):
+
+    #if the value of i becomes
+    #5 the program is forced
+    #to exit
+    if i == 5:
+                 
+            #print the exit message
+            print(exit)
+            exit()
+    print(i)
+    
