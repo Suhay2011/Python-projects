9d4ce57fac03a3a71f92e4555082aec2e6ab8ec0
Suhay2011 <suhayl.abrahams3@gmail.com>
2025-01-16 19:58:35 +0200

    parrot commit

---
+++ class parrot.py
@@ -0,0 +1,18 @@
+#create class
+class Parrot:
+
+    #class atribute
+    species = "bird"
+
+    #instance atribute
+def__init(self,name,age)
+    self.name = Name
+    slef.age = age
+
+#instantiate the Parrot class
+blu = Parrot("Blu" , 10)
+woo = Parrot("Woo" , 15)
+
+#access the class atributes
+print("Blu is a {}",format(blu.species))
+print("Woo is also a {}",format(woo.species))
