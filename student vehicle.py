e194e10337c7f860ab5bf4985d57be76ab7fc44b
Suhay2011 <suhayl.abrahams3@gmail.com>
2025-01-16 19:45:50 +0200

    student commit

---
+++ student vehicle.py
@@ -0,0 +1,16 @@
+#create class
+class Vehicle:
+     
+     #create init method
+  def_init_(self,max_speed,mileage)
+
+            #bind the arguments
+self.max_speed = max_speed
+self.mileage = mileage
+  
+#object creation
+modelX = Vehicle(240 , 18)
+
+#access the methodn inside init module
+print("Model Max Speed",modelX.max_speed )
+print("Model Mileage",modelX.mileage)
