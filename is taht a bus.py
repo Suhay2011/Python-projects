2fb6fd40a424f98eb2bb083556606d611c4b92aa
Suhay2011 <suhayl.abrahams3@gmail.com>
2025-01-23 20:29:30 +0200

    bus commit

--- is taht a bus.py
+++ is taht a bus.py
@@ -1,8 +1,10 @@
 class Vehicle:
 
     def __init__(self,name,max_speed,mileage):
-        self.name = nameself.max_speed = max_speedself.mileage = mileage
-    class Bus(Vehicle):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+class Bus(Vehicle):
         pass
 
 School_bus = Bus("School Volvo" ,180, 12)
